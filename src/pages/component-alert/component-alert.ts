00acba575754f5a46b1c2861326b0f012db387cc
Bikash <bikash11roy@gmail.com>
2017-04-01 03:40:25 +0600

    Prompt alert created

--- src/pages/component-alert/component-alert.ts
+++ src/pages/component-alert/component-alert.ts
@@ -28,4 +28,31 @@ export class ComponentAlertPage {
     alert.present();
   }
 
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Login',
+      message: "Enter a name for this new album you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
