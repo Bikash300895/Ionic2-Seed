41043b62e3ade212e5ea6fb6abacd54a001f487b
Bikash <bikash11roy@gmail.com>
2017-04-03 05:39:34 +0600

    Ragne component

--- src/pages/range/range.ts
+++ src/pages/range/range.ts
@@ -12,6 +12,11 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'range.html'
 })
 export class RangePage {
+  brightness: number = 20;
+  contrast: number = 0;
+  warmth: number = 1300;
+  structure: any = { lower: 33, upper: 60 };
+  text: number = 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
