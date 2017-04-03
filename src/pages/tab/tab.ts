b986dc345440be049fd17a0842365f686f3824c3
Bikash <bikash11roy@gmail.com>
2017-04-03 09:26:44 +0600

    Tab page with badges

--- src/pages/tab/tab.ts
+++ src/pages/tab/tab.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {ComponentBackgroundCardPage} from "../background-card/component-background-card";
 import {ComponentCardPage} from "../card/component-card";
+import {ModelPage} from "../modal/model";
 
 /*
   Generated class for the Tab page.
@@ -16,10 +17,12 @@ import {ComponentCardPage} from "../card/component-card";
 export class TabPage {
   tab1: any;
   tab2: any;
+  tab3: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.tab1 = ComponentBackgroundCardPage;
     this.tab2 = ComponentCardPage;
+    this.tab3 = ModelPage;
   }
 
   ionViewDidLoad() {
